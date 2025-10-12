67821749873713deed760ca490d65c31f8b2ec4f
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-10-12 21:41:49 +0300

    add undelete service

--- apps/balance/services/__init__.py
+++ apps/balance/services/__init__.py
@@ -1,11 +1,17 @@
 from .customer import add_customer, change_customer, delete_customer
-from .transaction import add_transaction, change_transaction, delete_transaction
+from .transaction import (
+    add_transaction,
+    change_transaction,
+    delete_transaction,
+    undelete_transaction,
+)
 
 __all__ = [
-  "add_customer",
-  "change_customer",
-  "delete_customer",
-  "add_transaction",
-  "change_transaction",
-  "delete_transaction",
+    "add_customer",
+    "change_customer",
+    "delete_customer",
+    "add_transaction",
+    "change_transaction",
+    "delete_transaction",
+    "undelete_transaction",
 ]
