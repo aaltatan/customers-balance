17815fae516693bed527f50ab9c5edca32cc12cc
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-10-12 21:41:27 +0300

    add undelete service

--- apps/balance/services/transaction.py
+++ apps/balance/services/transaction.py
@@ -1,5 +1,9 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db import transaction
+
 from apps.core.models import User
-from apps.core.protocols import SaverProtocol
+from apps.core.models.activity import Activity
+from apps.core.protocols import SaveableProtocol
 from apps.core.services import Service
 
 from ..models import Transaction, TransactionSerializer
@@ -9,14 +13,14 @@ class TransactionService(Service[Transaction]):
     model_class = Transaction
 
 
-def add_transaction(user: User, saver: SaverProtocol[Transaction]) -> Transaction:
+def add_transaction(user: User, saver: SaveableProtocol[Transaction]) -> Transaction:
     return TransactionService.add(user, saver)
 
 
 def change_transaction(
     user: User,
     instance: Transaction,
-    saver: SaverProtocol[Transaction],
+    saver: SaveableProtocol[Transaction],
 ) -> Transaction:
     return TransactionService.change(
         user=user,
@@ -29,4 +33,16 @@ def change_transaction(
 def delete_transaction(user: User, instance: Transaction) -> None:
     return TransactionService.delete(
         user, instance, serializer_class=TransactionSerializer
+    )
+
+
+@transaction.atomic
+def undelete_transaction(user: User, instance: Transaction) -> None:
+    instance.undelete()
+    Activity.objects.create(
+        user=user,
+        kind=Activity.KindChoices.OTHER,
+        object_id=instance.pk,
+        content_type=ContentType.objects.get_for_model(Transaction),
+        notes="UNDELETE",
     )
