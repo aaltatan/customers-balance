bf06a84f10f86af4f1325ffee9d5fbdff4b76bde
Abdullah Altatan <117931250+aaltatan@users.noreply.github.com>
2025-10-28 04:03:22 +0300

    add annotated net field

--- apps/balance/managers/transaction.py
+++ apps/balance/managers/transaction.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+from apps.core.dbfields import MoneyField
+
 
 class TransactionQueryset(models.QuerySet):
-    pass
+    def annotate_net(self) -> "TransactionQueryset":
+        return self.annotate(
+            net=models.Window(
+                expression=models.Sum(models.F("amount")),
+                order_by=models.F("date").asc(),
+                frame=models.RowRange(None, 0),
+                output_field=MoneyField(),
+            )
+        )
 
 
 class TransactionManager(models.Manager):
+    def annotate_net(self) -> TransactionQueryset:
+        return self.get_queryset().annotate_net()
+
     def get_queryset(self) -> TransactionQueryset:
         return (
             TransactionQueryset(self.model, using=self._db)
