7846e2af861e91fb7533082b5945d98cd308b102
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2025-11-11 09:11:27 +0200

    make modals focusable

--- assets/main.ts
+++ assets/main.ts
@@ -1,3 +1,4 @@
+import focus from "@alpinejs/focus";
 import mask from "@alpinejs/mask";
 import persist from "@alpinejs/persist";
 import Alpine, { type Alpine as AlpineType } from "alpinejs";
@@ -21,8 +22,9 @@ document.addEventListener("htmx:load", () => {
   htmx.config.historyRestoreAsHxRequest = false;
 });
 
-Alpine.plugin(persist);
+Alpine.plugin(focus);
 Alpine.plugin(mask);
+Alpine.plugin(persist);
 
 Alpine.data("colorTheme", data.colorTheme);
 Alpine.data("body", data.body);
